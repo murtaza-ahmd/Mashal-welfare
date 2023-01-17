218ebc9fd603ddf24d5aef36b78b0489aca511b6
Murtaza Ahmad <mrkhan7707@gmail.com>
2023-01-18 00:05:45 +0500

    navigationfinal

--- admin/parts/footer.php
+++ admin/parts/footer.php
@@ -13,18 +13,19 @@
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/popper.js/umd/popper.min.js"> </script>
 <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
-<script src="vendor/chart.js/Chart.min.js"></script>
+<!-- <script src="vendor/jquery.cookie/jquery.cookie.js"> </script> -->
+<!-- <script src="vendor/chart.js/Chart.min.js"></script> -->
 <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
-<script src="js/charts-home.js"></script>
+<!-- <script src="js/charts-home.js"></script> -->
 <script src="js/front.js"></script>
 <script>
   $(document).ready(function() {
+    $(window).on('hashchange', function() {
+      hashValueChange();
+    });
+
     $('.sidebar_menu a').click(function(e) {
-      // remove active class from all li items
-      $('.sidebar_menu li').removeClass('active');
-      // add active class to current clicked li item
-      $(this).parent().addClass('active');
+      activateLink($(this));
       // get url for clicked a tag
       var url = $(this).attr('href');
       // show loader
@@ -43,21 +44,41 @@
 
 
     });
-
-    var urlHash = window.location.hash.replace('#', '');
-    if (!urlHash) {
-      urlHash = 'home_profile';
+    
+    function activateLink(anchor) {
+      $('.sidebar_menu li').removeClass('active');
+      // add active class to current clicked li item
+      $(anchor).parent().addClass('active');
     }
-    console.log(urlHash);
-    $('.page-content').html("<div style='position:relative; height:400px; width:1000px; text-align:center;'><img style='position:absolute; top:0; bottom:0; margin:auto' src='../assets/img/loader.gif' /></div>");
-    // make an ajax call and get html of child container
-    $.ajax({
-      url: urlHash + '.php',
-      success: function(result) {
 
-        $(".page-content").html(result);
+    function hashValueChange() {
+
+      var urlHash = window.location.hash.replace('#', '');
+      if (!urlHash) {
+        urlHash = 'home_profile';
       }
-    });
+
+      urlHash = urlHash + '.php';
+
+      // The .each() method is unnecessary here:
+      $(".sidebar_menu a").each(function() {
+        if ($(this).attr('href') === urlHash) {
+          activateLink($(this))
+        }
+      });
+
+      $('.page-content').html("<div style='position:relative; height:400px; width:1000px; text-align:center;'><img style='position:absolute; top:0; bottom:0; margin:auto' src='../assets/img/loader.gif' /></div>");
+      // make an ajax call and get html of child container
+      $.ajax({
+        url: urlHash,
+        success: function(result) {
+
+          $(".page-content").html(result);
+        }
+      });
+    }
+    hashValueChange();
+
   });
   // $(document).ready(function(){
   //   alert('hello');
